1b9bf3e0e16075956b5cbd6dafa1250b04ebd8ba
Scott McKean <scott.mckean@ucalgary.ca>
2022-09-24 14:14:06 -0600

    Developing figures for paper

--- case_studies/duvernay/connectivity_analysis/connectivity_grid_search.py
+++ case_studies/duvernay/connectivity_analysis/connectivity_grid_search.py
@@ -11,21 +11,20 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
 # make sure we are in the right directory
+case = "duvernay"
+run_simulations = False
 os.chdir(
-    r"C:\repos\fracture_intensity_inference\case_studies\montney\connectivity_analysis"
+    f"C:/repos/fracture_intensity_inference/case_studies/{case}/connectivity_analysis"
 )
 
-run_grid_search = False
-
 # setup the grid
-p32_search = [1e-3, 2e-3, 3e-3, 4e-3, 5e-3, 6e-3, 7e-3, 8e-3, 9e-3, 10e-3]
-min_length_search = [25, 50, 75, 100, 150, 200, 250, 300, 400, 500]
+if run_simulations:
+    p32_search = [1e-3, 2e-3, 3e-3, 4e-3, 5e-3, 6e-3, 7e-3, 8e-3, 9e-3, 10e-3]
+    min_length_search = [25, 50, 75, 100, 150, 200, 250, 300, 400, 500]
+    p32s, min_lengths = np.meshgrid(p32_search, min_length_search, sparse=False)
+    search_runs = np.array([p32s, min_lengths]).reshape(2, -1).T
 
-p32s, min_lengths = np.meshgrid(p32_search, min_length_search, sparse=False)
-search_runs = np.array([p32s, min_lengths]).reshape(2, -1).T
-
-# run everything
-if run_grid_search:
+    # run everything
     for i in range(0, search_runs.shape[0]):
         print(i, search_runs[i, 0], search_runs[i, 1])
         cmd = f"python run_connectivity.py --run {i} --p32 {search_runs[i,0]} --min_length {search_runs[i,1]}"
@@ -37,43 +36,12 @@ run_summaries = pd.concat(
     [pd.read_parquet(f) for f in Path(".").rglob("cluster_summary*.parquet")]
 )
 
-run_summaries.sort_values("num_lineaments").to_csv("run_summaries.csv")
-
-
 sns.set_style("whitegrid")
 
 fig, ax = plt.subplots()
 ax = sns.heatmap(
     run_summaries.pivot("p32", "min_length", "total_length"),
     cbar_kws={"label": "Total Connected Fracture Length"},
-    cmap="viridis",
-    square=True,
-    fmt="g",
-)
-ax.invert_yaxis()
-plt.yticks(rotation=0)
-plt.ylabel("P32 (m$^{-1}$)")
-plt.xlabel("Minimum Fracture Length (m)")
-plt.show(block=False)
-
-fig, ax = plt.subplots()
-ax = sns.heatmap(
-    run_summaries.pivot("p32", "min_length", "clusters"),
-    cbar_kws={"label": "Number of Connected Clusters"},
-    cmap="cividis",
-    square=True,
-    fmt="g",
-)
-ax.invert_yaxis()
-plt.yticks(rotation=0)
-plt.ylabel("P32 (m$^{-1}$)")
-plt.xlabel("Minimum Fracture Length (m)")
-plt.show(block=False)
-
-fig, ax = plt.subplots()
-ax = sns.heatmap(
-    run_summaries.pivot("p32", "min_length", "num_lineaments"),
-    cbar_kws={"label": "Number of Connected Lineaments"},
     cmap="plasma",
     square=True,
     fmt="g",
@@ -82,4 +50,38 @@ ax.invert_yaxis()
 plt.yticks(rotation=0)
 plt.ylabel("P32 (m$^{-1}$)")
 plt.xlabel("Minimum Fracture Length (m)")
+plt.savefig(f"{case}_connected_fracture_length.pdf")
+plt.savefig(f"{case}_connected_fracture_length.png")
+plt.show(block=False)
+
+fig, ax = plt.subplots()
+ax = sns.heatmap(
+    run_summaries.pivot("p32", "min_length", "clusters"),
+    cbar_kws={"label": "Number of Connected Clusters"},
+    cmap="viridis",
+    square=True,
+    fmt="g",
+)
+ax.invert_yaxis()
+plt.yticks(rotation=0)
+plt.ylabel("P32 (m$^{-1}$)")
+plt.xlabel("Minimum Fracture Length (m)")
+plt.savefig(f"{case}_connected_clusters.pdf")
+plt.savefig(f"{case}_connected_clusters.png")
+plt.show(block=False)
+
+fig, ax = plt.subplots()
+ax = sns.heatmap(
+    run_summaries.pivot("p32", "min_length", "num_lineaments"),
+    cbar_kws={"label": "Number of Connected Lineaments"},
+    cmap="cividis",
+    square=True,
+    fmt="g",
+)
+ax.invert_yaxis()
+plt.yticks(rotation=0)
+plt.ylabel("P32 (m$^{-1}$)")
+plt.xlabel("Minimum Fracture Length (m)")
+plt.savefig(f"{case}_connected_lineaments.pdf")
+plt.savefig(f"{case}_connected_lineaments.png")
 plt.show(block=False)
